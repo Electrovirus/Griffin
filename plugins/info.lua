5ec8758843dc131d1f74e40f6639bba792dad071
Dr0virus <e.nourifard110@gmail.com>
2016-04-29 22:25:11 +0430

    Delete info.lua

--- plugins/info.lua
+++
@@ -1,2 +0,0 @@
-
-#addplug do function run(msg, matches) local reply_id = msg['id'] local info = '⚠Name : '..msg.from.first_name..'\n\n' ..'🔰Id : '..msg.from.id..'\n' ..'🔰Username : @'..msg.from.username..'\n\n' ..'🔰Group Id : '..msg.to.id..'\n' ..'🔰Group name : '..msg.to.title reply_msg(reply_id, info, ok_cb, false) end return { patterns = { "^[!/#]info" }, run = run } end info
